13eff0453ee2531001c5372313d91e4fd112df91
Bryan Eng <eng.w.bryan@gmail.com>
2019-02-14 19:25:55 -0800

    Added "add task" input and button

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,8 +7,20 @@ import { Component } from '@angular/core';
 
 export class Tab1Page {
   toDoList: string[] = [];
+  newTask: string;
   constructor() {
+    this.newTask = '';
     this.toDoList = JSON.parse(localStorage.getItem('todolist'));
     // localStorage.setItem('todolist', JSON.stringify(this.toDoList));
+  }
+
+  // ionViewDidLoad() {
+  //   this.toDoList = JSON.parse(localStorage.getItem('todolist'));
+  //   console.log('something changed!');
+  // }
+
+  addNewTask() {
+    this.toDoList.push(this.newTask);
+    localStorage.setItem('todolist', JSON.stringify(this.toDoList));
   }
 }
